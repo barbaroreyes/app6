85cec1110a6a9aec8525057a0f6e61b95c294751
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-06-16 20:04:51 -0500

    added-auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import {Switch,Route, Link} from 'react-router-dom'
 import Amplify ,{API,graphqlOperation}from 'aws-amplify';
 import awsconfi from './aws-exports'
 import {AmplifySignOut,withAuthenticator}from '@aws-amplify/ui-react'
@@ -7,6 +8,12 @@ Amplify.configure(awsconfi)
 function App() {
   return (
     <div className="App">
+      <Link to='/home'></Link>
+      <Switch>
+        <Route exact path="/">
+         hello
+        </Route>
+      </Switch>
       <AmplifySignOut/>
         <h1>My app</h1>
     </div>
